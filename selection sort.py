3e661311cdec6c6a037fd58f97d75e4ef9145d6e
mallidisrilakshmisaranya <164532327+mallidisrilakshmisaranya@users.noreply.github.com>
2024-05-30 15:15:47 +0530

    Add files via upload

---
+++ selection sort.py
@@ -0,0 +1,20 @@
+def performSelectionSort(nums):
+    n=len(nums)
+    fixThisIndex=n-1
+    while fixThisIndex>0:
+        maxEleIndex=fixThisIndex
+        
+        #finding the maximum element index
+        for index in range(fixThisIndex):
+            if nums[index]>nums[maxEleIndex]:
+                maxEleIndex=index
+        if maxEleIndex!=fixThisIndex:   
+            temp=nums[maxEleIndex]
+            nums[maxEleIndex]=nums[fixThisIndex]
+            nums[fixThisIndex]=temp
+        print(nums)
+        fixThisIndex-=1
+nums=[10,8,2,14,12,7]
+print("Before sorting:",nums)
+performSelection Sort(nums)
+print("After sorting:",nums)
