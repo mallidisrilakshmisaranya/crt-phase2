baaec3e00dced040e16515227dcc59623276cce7
mallidisrilakshmisaranya <164532327+mallidisrilakshmisaranya@users.noreply.github.com>
2024-05-31 10:30:43 +0530

    Add files via upload

---
+++ inserting and deleting elements from stack.py
@@ -0,0 +1,21 @@
+def push(stack,ele):
+    stack.append(ele)
+    print(ele,"inser=-ted into stack successfully")
+def pop(stack):
+    if len (stack)==0:
+        print("stack is empty")
+        return
+    print(stack[-1],"deleted successfully")
+    stack.pop()
+stack=[]
+push(stack,10)
+push(stack,20)
+push(stack,30)
+push(stack,40)
+push(stack,50)
+pop(stack)
+pop(stack)
+pop(stack)
+pop(stack)
+pop(stack)
+pop(stack)
